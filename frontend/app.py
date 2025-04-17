90c2d0e12b8c91db141c8d0ed65b9a83135c7b95
Radi Aman Riyas <radi.riyas@ghaf.ai>
2025-04-17 20:34:30 +0400

    add: basic ui

---
+++ frontend/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(page_title="WatchGrid", layout="wide")
+
+st.title("📽️WatchGrid")
+st.subheader("Your personal movie space. Coming Soon!")
+
+st.markdown("""
+
+This app lets you:
+- Track what you've watched
+- Create Custom Watch Lists
+- Rate and Review Movies
+- Find out where you're favorite shows are being streamed!
+""")
