87edc9d9199d8fe7d595f1b6c837194d7af6a1db
Radi Aman Riyas <radi.riyas@ghaf.ai>
2025-04-18 23:28:48 +0400

    fix: introduce small delay to load images

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from models.movie import Movie
 from core.settings import settings
 import sys
 import os
-
+import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 st.set_page_config(page_title="WatchGrid")
@@ -26,6 +26,13 @@ movies = asyncio.run(fetch_movies())
 cols = st.columns(5)
 for idx, movie in enumerate(movies):
     with cols[idx % 5]:
-        st.image(movie.image_url, width=150)
+        try:
+            # try to load the img one by one w some small delay
+            time.sleep(0.2)
+            st.image(movie.image_url, width=150)
+        except:
+            st.image("https://via.placeholder.com/150?text=No+Image", width=150)
+
+        # Text info
         st.markdown(f"**{movie.title}**")
         st.caption(f"📅 {movie.release_date} | 🌐 {movie.language} | ⭐ {movie.avg_rating}")
